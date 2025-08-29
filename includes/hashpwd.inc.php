3c54bacac0f1ff966face9db999873af5392d564
Victor <thehunter.modd@gmail.com>
2025-08-29 14:04:15 +0100

    third initialization

--- includes/hashpwd.inc.php
+++ includes/hashpwd.inc.php
@@ -1,11 +1,35 @@
 <?php
 
-$sesitiveData = 'Victor';
+$sesitiveData = 'Oyeleke';
 $salt = bin2hex(random_bytes(16)); //Generate random salt
-$pepper = "AsecretPepperString";  
-echo "<br>". $salt;     
+$pepper = "ASecretPepperString";  
+//echo "<br>". $salt;     
 
 $dataTOHash = $sesitiveData. $salt. $pepper;
 $hash = hash('sha256',$dataTOHash );
 
-echo "<br>". $hash;
+//echo "<br>". $hash;
+
+/*-----*/
+$sesitiveData = 'Oyeleke';
+
+$storedSalt = $salt;
+$storedHash = $hash;
+$pepper = "ASecretPepperString";
+
+$dataTOHash = $sesitiveData. $storedSalt. $pepper;
+
+$verificationHash = hash('sha256',$dataTOHash );
+
+
+if ($storedHash === $verificationHash) {
+    echo "The data are the same!";
+    echo "<br>";
+    echo $storedHash;
+    echo "<br>";
+    echo $verificationHash;
+}
+else {
+    echo "The data are NOT the same!";
+}
+
